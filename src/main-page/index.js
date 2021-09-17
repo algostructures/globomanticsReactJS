c9fc025afa1baf2fbe350bb8979965463bbe2a0e
Saurabh Prajapati <saurabh945644@gmail.com>
2021-09-17 21:34:57 +0530

    completed module 5

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,24 +1,53 @@
-import logo from "./logo.svg";
+import { useState, useEffect, useMemo } from "react";
 import "./main-page.css";
+import Header from "./header";
+import FeaturedHouse from "./featured-house";
+import HouseFilter from "./house-filter";
+import SearchResults from "../search-results";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HouseFromQuery from "../house/HouseFromQuery";
 
 function App() {
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Header subtitle="Providing houses all over the world" />
+        <HouseFilter allHouses={allHouses} />
+
+        <Switch>
+          <Route path="/searchresults/:country">
+            <SearchResults allHouses={allHouses} />
+          </Route>
+
+          <Route path="/house/:id">
+            <HouseFromQuery allHouses={allHouses} />
+          </Route>
+
+          <Route path="/">
+            <FeaturedHouse house={featuredHouse} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
